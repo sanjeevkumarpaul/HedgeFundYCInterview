b30872051958f92a26ca22890cb7e7a27a645753
Sanjeev Paul <SanjeevKumarPaul@hotmail.com>
2017-11-16 13:55:55 -0500

    jsut a start

--- YorkCapitalConsole/YorkCapitalConsole/YCStart.cs
+++ YorkCapitalConsole/YorkCapitalConsole/YCStart.cs
@@ -10,6 +10,29 @@ namespace YorkCapitalConsole
     {
         static void Main(string[] args)
         {
+            var _console = new YCConsole();
+            
+            Console.WriteLine($"Prime check of 121 : {_console.IsPrime(121)}");
+            Console.WriteLine($"Prime check of 3 : {_console.IsPrime(3)}");
+
+            Console.ReadLine();
         }
     }
+
+    class YCConsole
+    {
+        public bool IsPrime(long value)
+        {
+            bool _flag = true;
+
+            for (long index = value - 1; index > 1; index--)
+            {
+                if (value % index == 0) { _flag = false; break; }
+            }
+
+            return _flag;
+        }
+
+    }
+
 }
