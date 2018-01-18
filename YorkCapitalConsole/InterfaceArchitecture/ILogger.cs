0c8f742c5701c78ab26da998551dcbac1ecf3378
Sanjeev Paul <SanjeevKumarPaul@hotmail.com>
2018-01-17 21:36:48 -0500

    Interfaces

--- YorkCapitalConsole/InterfaceArchitecture/ILogger.cs
+++ YorkCapitalConsole/InterfaceArchitecture/ILogger.cs
@@ -9,6 +9,8 @@ namespace InterfaceArchitecture
     public interface ILogger
     {
         void Info(string text);
-
+        void Warning(string text);
+        void Exception(Exception e);
+        void All(string text, Exception e = null);
     }
 }
