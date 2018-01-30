e976f9b400dc925cfaa86f5c14358e1465ac106f
Sanjeev Paul <SanjeevKumarPaul@hotmail.com>
2018-01-30 15:41:13 -0500

    Learning how to create a nuget package.

---
+++ YorkCapitalConsole/NugetPackageLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetPackageLibrary
+{
+    // https://blogs.msdn.microsoft.com/mvpawardprogram/2016/06/28/creating-nuget-packages/
+
+    public class Class1
+    {
+        
+    }
+}
