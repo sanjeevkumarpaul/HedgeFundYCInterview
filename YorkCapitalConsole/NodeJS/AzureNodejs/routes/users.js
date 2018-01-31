94406c3c79a4f778fc66da7469a83553ace81ff1
Sanjeev Paul <SanjeevKumarPaul@hotmail.com>
2018-01-31 17:21:32 -0500

    Nodejs Impletation with Handlebarsjs view Engine. How to change the view engine in app.js and tiny little tings with nod js.

---
+++ YorkCapitalConsole/NodeJS/AzureNodejs/routes/users.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+var getData = function () {
+    var data = {
+        'item1': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-76.jpg',
+        'item2': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-77.jpg',
+        'item3': 'http://public-domain-photos.com/free-stock-photos-1/flowers/cactus-78.jpg'
+    }
+    return data;
+}
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    //res.send('respond with a resource');
+    res.render('Usersview', { title: 'From user level- Node JS', "data": getData() });    
+});
+
+module.exports = router;
---
+++ YorkCapitalConsole/NodeJS/FirstNodeJs/ConsoleApp.js
@@ -0,0 +1,4 @@
+﻿
+var msg = 'Hello World';
+console.log(msg);
+
