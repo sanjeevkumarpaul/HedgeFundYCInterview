806dc26e26505f2062f3eed6aba0f4ef60a77d9d
Sanj <SanjeevKumarPaul@hotmail.com>
2018-06-01 08:25:03 -0400

    Separare APINode base

--- YorkCapitalConsole/APICalls/Configurations/APIXmlNode.cs
+++ YorkCapitalConsole/APICalls/Configurations/APIXmlNode.cs
@@ -9,25 +9,28 @@ using Extensions;
 
 namespace APICalls.Configurations
 {
-    internal class APIXmlNode
+    public class APINodeBase
     {
-        internal string Name { get; set; }
-        internal string GenericType { get; set; }
-        internal string BaseUrl { get; set; }
-        internal string ApiUri { get; set; }
-        internal bool RequiredAuthorization { get; set; }
-        internal APIAuthenticationType AuthenticationType { get; set; }
-        internal string Token { get; set; }
-        internal bool TokenAsHeader { get; set; }
-        internal Dictionary<string, string> Parameters { get; set; }
-        internal Dictionary<string, string> Headers { get; set; }
+        public string Name { get; set; }
+        public string GenericType { get; set; }
+        public string BaseUrl { get; set; }
+        public string ApiUri { get; set; }
+        public bool RequiredAuthorization { get; set; }
+        public APIAuthenticationType AuthenticationType { get; set; }
+        public string Token { get; set; }
+        public bool TokenAsHeader { get; set; }
+        public Dictionary<string, string> Parameters { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
         /// <summary>
         /// Sepration to be done via semicolon at XML
         /// application/json[;application/jpeg[;...]]
         /// </summary>
-        internal string ContentType { get; set; }
-        internal APIMethod Method { get; set; }
+        public string ContentType { get; set; }
+        public APIMethod Method { get; set; }
+    }
 
+    internal class APIXmlNode : APINodeBase
+    {       
         internal IAPIProspect Result { get; set; }
 
         internal APIXmlNode(XElement element, string baseUrl)
