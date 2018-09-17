ac16fdf0e802e5592f295b0efb9192624a4ff4eb
sanjeevkumarpaul <sanjeevkumarpaul@users.noreply.github.com>
2018-09-17 10:41:11 -0400

    Write to Excel as console Table.

--- YorkCapitalConsole/Enhancers/Outputers/WrapExcelTable.cs
+++ YorkCapitalConsole/Enhancers/Outputers/WrapExcelTable.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Wrappers.Outputers
 {
-    class WrapExcelTable
+    public partial class WrapExcelTable : _BaseOutputTable
     {
+        public WrapExcelTable(WrapOutputerOptions options) : base(options) { }
+        public WrapExcelTable(ConsoleTable table, WrapOutputerOptions options) : base(table, options) { }
+        public WrapExcelTable(List<ConsoleTable> tables, WrapOutputerOptions options) : base(tables, options) { }
+
+        protected override void Init() { }
+        protected override void Start() { }
+        protected override void PutTable()
+        { }
+        protected override void Finish() { }
+    }
+
+    partial class WrapExcelTable
+    {
+        #region ^Save the HTML output to a file
+        private void SaveToDisk()
+        {            
+            
+        }
+        #endregion ~END OF Save the HTML output to a file
     }
 }
