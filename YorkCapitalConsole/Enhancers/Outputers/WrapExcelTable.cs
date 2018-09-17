6088154644a20e2287188ecd755bba02561f7b4f
sanjeevkumarpaul <sanjeevkumarpaul@users.noreply.github.com>
2018-09-17 13:32:06 -0400

    excel coming

--- YorkCapitalConsole/Enhancers/Outputers/WrapExcelTable.cs
+++ YorkCapitalConsole/Enhancers/Outputers/WrapExcelTable.cs
@@ -15,17 +15,95 @@ namespace Wrappers.Outputers
         protected override void Init() { }
         protected override void Start() { }
         protected override void PutTable()
-        { }
+        {
+            using (OpenExcel()) //Opens Excel as Memory Stream.
+            {
+                CreateColumnHeaders();
+            }
+        }
+        protected override void Finish()
+        {            
+            xl.Document.SaveAs(_path);
+
+            xl.DisposeOff();
+        }
         protected override void Finish() { }
     }
 
     partial class WrapExcelTable
     {
-        #region ^Save the HTML output to a file
-        private void SaveToDisk()
-        {            
-            
+        private class Excel : IDisposable
+        {
+            internal MemoryStream Stream { get; set; }
+            internal SpreadsheetDocument Document { get; set; }
+            internal WorkbookPart BookPart { get; set; }
+            internal WorksheetPart SheetPart { get; set; }
+            internal SheetData Data { get; set; }
+            internal Sheets Sheets { get; set; }
+            internal Sheet Current { get; set; }
+
+            public void Dispose()
+            {
+               //Only to facilitiate Using() 
+            }
+
+            public void DisposeOff()
+            {
+                if (Stream != null) Stream.Close();
+                Stream = null;
+            }
         }
-        #endregion ~END OF Save the HTML output to a file
+                
+        private Excel OpenExcel()
+        {
+            if (!_InProgress || xl.Stream == null)
+            {
+                xl.Stream = new MemoryStream();
+                xl.Document = SpreadsheetDocument.Create(xl.Stream, SpreadsheetDocumentType.Workbook);
+
+                xl.BookPart = xl.Document.AddWorkbookPart();
+                xl.BookPart.Workbook = new Workbook();
+                xl.SheetPart = xl.BookPart.AddNewPart<WorksheetPart>();
+                xl.Data = new SheetData();
+
+                xl.SheetPart.Worksheet = new Worksheet(xl.Data);
+
+                xl.Sheets = xl.Document.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+
+                xl.Current = new Sheet()
+                {
+                    Id = xl.Document.WorkbookPart.GetIdOfPart(xl.SheetPart),
+                    SheetId = 1,
+                    Name = $"{OutOption.Name}"
+                };
+                xl.Sheets.AppendChild(xl.Current);
+            }
+            return xl;
+        }
+
+        private void CreateColumnHeaders()
+        {
+            UInt32 rowIdex = 0;
+            var row = new Row { RowIndex = ++rowIdex };
+            xl.Data.AppendChild(row);
+
+            if (_table.OtherOptions.IsFirstRowAsHeader)
+                _table.Rows.Take(1).ElementAt(0).Column.ForEach(c => row.AppendChild( CreateTextCell(c.Text) ));
+        }
+
+        private Cell CreateTextCell(string text)
+        {
+            var cell = new Cell
+            {
+                DataType = CellValues.InlineString,
+                CellReference = text.GetHashCode().ToString()
+            };
+
+            var istring = new InlineString();
+            var t = new Text { Text = text };
+            istring.AppendChild(t);
+            cell.AppendChild(istring);
+            return cell;
+        }
     }
 }
