77be198786bf13fff1e6745122ae398733b43149
Sanj <SanjeevKumarPaul@hotmail.com>
2018-09-20 17:30:01 -0400

    open xml wrapper

--- YorkCapitalConsole/Enhancers/WrapOpenXml.cs
+++ YorkCapitalConsole/Enhancers/WrapOpenXml.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Wrappers
 {
