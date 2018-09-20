4c834d48db8f860cb5cea7aa49a6cff30b15e311
sanjeevkumarpaul <sanjeevkumarpaul@users.noreply.github.com>
2018-09-20 17:28:25 -0400

    Get Format Mappings

--- YorkCapitalConsole/Enhancers/WrapOpenXml.cs
+++ YorkCapitalConsole/Enhancers/WrapOpenXml.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Wrappers
 {
-    class WrapOpenXml
+    public static class WrapOpenXml
     {
+        public static Dictionary<uint, String> GetFormatMappings(SpreadsheetDocument document)
+        {
+            Dictionary<uint, String> formatMappings = new Dictionary<uint, String>();
+
+            var stylePart = document.WorkbookPart.WorkbookStylesPart;
+
+            var numFormatsParentNodes = stylePart.Stylesheet.ChildElements.OfType<NumberingFormats>();
+
+            foreach (var numFormatParentNode in numFormatsParentNodes)
+            {
+                var formatNodes = numFormatParentNode.ChildElements.OfType<NumberingFormat>();
+                foreach (var formatNode in formatNodes)
+                {
+                    formatMappings.Add(formatNode.NumberFormatId.Value, formatNode.FormatCode);
+                }
+            }
+            
+            return formatMappings;
+        }
     }
 }
